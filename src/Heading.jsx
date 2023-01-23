e8eec4ae795bf469d2fc531e3c966883d74af10a
ZAMSOFTS <zaeembestplayer@gmail.com>
2023-01-23 16:27:43 +0400

    First
    I am not proud

---
+++ src/Heading.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import css from "./index.css";
+
+let datu = new Date();
+datu = datu.getHours();
+
+let greeting = "";
+const ztile = {};
+
+if (datu > 4 && datu < 12) {
+  greeting = "Good Morning";
+  ztile.color = "green";
+} else if (datu >= 12 && datu < 18) {
+  greeting = "Good Afternoon";
+  ztile.color = "red";
+} else if (datu >= 18 && datu <= 24) {
+  greeting = "Good Night";
+  ztile.color = "black";
+}
+
+function Heading() {
+  return (
+    <h1 className="stal" contentEditable>
+      Hello Sir ,<span style={ztile}>{greeting}</span>
+    </h1>
+  );
+}
+
+export default Heading;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import bg from "./images/24.png";
+
+ReactDOM.render(<App />, document.getElementById("root"));
