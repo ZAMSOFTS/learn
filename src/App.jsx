6a5e900da6100d3729ed3854e133a609a67080da
ZAMSOFTS <zaeembestplayer@gmail.com>
2023-01-25 09:59:38 +0400

    Naats

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import Heading from "./Heading";
+import "./index.css";
 
-function App() {
-  return <Heading />;
+function App(props) {
+  console.log(props);
+  return (
+    <>
+      <div className="cards">
+        <div className="card">
+          <img src={props.pic} className="cardImg" />
+          <div className="cardInfo">
+            <span className="cardCategory"> {props.cat}</span>
+            <h3 className="cardTitle">{props.tit}</h3>
+            <a href={props.link} target="_blank">
+              <button>Click To Watch</button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import bgvid from "./images/cricket.mp4";
-import css from "./index.css";
 
 ReactDOM.render(
   <>
-    <div className="vid">
-      <video autoPlay loop muted>
-        <source src={bgvid} type="video/mp4" />
-      </video>
-    </div>
-    <App />
+    <App
+      pic="https://i.ytimg.com/vi/L7dCk09AwWs/hq720.jpg?sqp=-oaymwE2COgCEMoBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAs4FigIMCAAQARhUIGUoZTAP&rs=AOn4CLC6zTLfS-pp0hx7UBYojRdg9OZ0jQ"
+      cat="Urdu Naats"
+      tit="Kabe Ki Ronak"
+      link="https://youtu.be/L7dCk09AwWs"
+    />
+    <App
+      pic="https://i.ytimg.com/vi/TfcjkaRpRUs/hqdefault.jpg?sqp=-oaymwE2CPYBEIoBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARgzIGQocjAP&rs=AOn4CLCiadeGHkQG99_xNkIMlOeAvARA4g"
+      cat="Urdu Naats"
+      tit="Nabi Ka Lab Par "
+      link="https://youtu.be/TfcjkaRpRUs"
+    />
+    <App
+      pic="https://i.ytimg.com/vi/dJ3wTqTmnGI/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB6UZSLf63qv38F6ezbRgDhaKQ0kw"
+      cat="Urdu Naats"
+      tit="Main Kaabe Ko Dekhunga "
+      link="https://youtu.be/dJ3wTqTmnGI"
+    />
+    <App
+      pic="https://i.ytimg.com/vi/yrQ_zAy_QYY/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBwImhEe48S3VpE4EV4_njTKQ_VaA"
+      cat="Urdu Naats"
+      tit="chor fikr duniya ki "
+      link="https://youtu.be/yrQ_zAy_QYY"
+    />
+    <App
+      pic="https://i.ytimg.com/vi/fvf3e1Ae9w4/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC3cNfRQly969kS5qdPzRXY5uDd0w"
+      cat="Urdu Naats"
+      tit="Wo Mera Nabi Hai "
+      link="https://youtu.be/fvf3e1Ae9w4"
+    />
   </>,
   document.getElementById("root")
 );
