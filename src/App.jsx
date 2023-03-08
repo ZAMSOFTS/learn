10a972e8ba0352ce0cda522d36e6e8bd66c3d168
ZAMSOFTS <zaeembestplayer@gmail.com>
2023-03-08 10:43:51 +0400

    haha

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,97 @@
 import React, { useState } from "react";
 import "./index.css";
+import myAudio from "./images/final.mp3";
 
 const App = () => {
-  const [before, after] = useState();
-  const [fullname, setfullname] = useState();
-  const [head, sethead] = useState("Lets Find Your Age");
-  const [iftar, setiftar] = useState();
+  const [fullName, setFullName] = useState({
+    fName: "",
+    lName: "",
+    eMail: "",
+    pNumber: "",
+  });
+
+  const onSubmit = (event) => {
+    const audio = new Audio(myAudio);
+    audio.play();
+    event.preventDefault();
+  };
 
   const inputEvent = (event) => {
-    after(event.target.value);
+    console.log(event.target.value);
+    console.log(event.target.name);
+
+    const { value, name } = event.target;
+
+    setFullName((preValue) => {
+      if (name === "fName") {
+        return { fName: value, lName: preValue.lName };
+      } else if (name === "lName") {
+        return {
+          fName: preValue.fName,
+          lName: value,
+          eMail: preValue.eMail,
+          pNumber: preValue.pNumber,
+        };
+      } else if (name === "eMail") {
+        return {
+          fName: preValue.fName,
+          lName: preValue.lName,
+          eMail: value,
+          pNumber: preValue.pNumber,
+        };
+      } else if (name === "pNumber") {
+        return {
+          fName: preValue.fName,
+          lName: preValue.lName,
+          eMail: preValue.eMail,
+          pNumber: value,
+        };
+      }
+    });
   };
 
-  const onSubmit = () => {
-    setfullname(before);
-    sethead("Your Are ");
-    setiftar(" Years Old");
-  };
   return (
-    <>
-      <div>
-        <h1>
-          {head} <span>{fullname}</span>
-          {iftar}
-        </h1>
-        <input
-          type="number"
-          pattern="[0-9]*"
-          max={12}
-          placeholder="Enter Your Age"
-          onChange={inputEvent}
-          value={before}
-        />
-        <button onClick={onSubmit}>Find Age</button>
+    <div class="container">
+      <div class="form-container">
+        <form onSubmit={onSubmit}>
+          <div>
+            <h1>
+              Hello {fullName.fName} {fullName.lName}
+            </h1>
+            <p class="info">Tusana Email: {fullName.eMail}</p>
+            <p class="info">Tusana Number: {fullName.pNumber}</p>
+            <input
+              placeholder="Enter Your First Name"
+              name="fName"
+              onChange={inputEvent}
+              value={fullName.fName}
+            />
+            <br />
+            <input
+              placeholder="Enter Your Last Name"
+              name="lName"
+              onChange={inputEvent}
+              value={fullName.lName}
+            />
+            <br />
+            <input
+              placeholder="Enter Your Email"
+              name="eMail"
+              onChange={inputEvent}
+              value={fullName.eMail}
+            />
+            <br />
+            <input
+              placeholder="Enter Your Phone Number"
+              name="pNumber"
+              onChange={inputEvent}
+              value={fullName.pNumber}
+            />
+            <button type="submit">Submit</button>
+          </div>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
