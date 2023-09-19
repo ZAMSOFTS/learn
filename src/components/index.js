fd3825977658dd9517cb16b974efb80d859834f5
ZAMSOFTS <123354162+ZAMSOFTS@users.noreply.github.com>
2023-09-19 23:22:14 +0400

    1

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import GetStarted from "./GetStarted";
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Client from "./Client";
+import CTA from "./CTA";
+import FeedBackCard from "./FeedBackCard";
+import Footer from "./Footer";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+  Navbar,
+  Hero,
+  GetStarted,
+  Billing,
+  Business,
+  Button,
+  CardDeal,
+  Client,
+  CTA,
+  FeedBackCard,
+  Footer,
+  Stats,
+  Testimonials,
+};
